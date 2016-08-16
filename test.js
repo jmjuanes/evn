28c84aac54a3464aeb54a25ddbc0697df6a512f3
jmjuanes <josemijuanes@gmail.com>
2016-08-16 19:19:24 +0200

    test.js: updated test

--- test.js
+++ test.js
@@ -13,6 +13,20 @@ function Test(id)
 
 var t1 = new Test('Test 1');
 
+//First call
+console.log('Normal call');
 t1.call('Hey');
+
+//ADd the new event
 Evn.add('test', t1.call, t1);
+
+//Second call
+console.log('Event call');
+Evn.send('test', 'Hello!!');
+
+//Remove the event
+Evn.remove('test', t1.call, t1);
+
+//Last call
+console.log('No event call');
 Evn.send('test', 'Hello!!');
